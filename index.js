7a886c319f922467f34b27ece32381fbfc90d8c6
rayjamil <ryanwekesa019@gmail.com>
2024-12-20 10:49:30 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return nfetch("https://anapioficeandfire.com/api/books/")
+  .then(response => response.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -9,6 +10,7 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    // console.log(books)
   });
 }
 
