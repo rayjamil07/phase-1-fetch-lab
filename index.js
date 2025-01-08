1a426a4129a681ff99fd48ebebb9f8f3c5c7ab5f
rayjamil <ryanwekesa019@gmail.com>
2025-01-08 10:41:26 +0300

    Adds one file

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books/")
-  .then(response => response.json())
-  .then(json => renderBooks(json))
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const html = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-    // console.log(books)
-  });
+    const h3 = document.createElement('h3')
+    h2.innerHTML = book.name
+    html.appendChild(h3)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
